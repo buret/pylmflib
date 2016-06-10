c8e2ab517b4d8e25a3d77e8e1b49205dde05af14
Celine Buret <buret.celine@gmail.com>
2016-06-10 10:59:46 +0100

    [examples] Remove '^' character from Mwotlap dictionary

--- examples/mwotlap/setting.py
+++ examples/mwotlap/setting.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from config.mdf import ps_partOfSpeech, mdf_lmf, lmf_mdf
+from config.mdf import ps_partOfSpeech, mdf_lmf, lmf_mdf, mdf_semanticRelation, pd_grammaticalNumber
 from common.range import partOfSpeech_range
 from utils.io import ENCODING
 
@@ -65,7 +65,7 @@ pdl = [
 
 ## Functions to process some MDF fields (input)
 def remove_char(value):
-    """Function to remove '^' character from definition MDF fields.
+    """Function to remove '^' character from MDF fields.
     """
     return value.replace('^','')
 
@@ -73,7 +73,7 @@ mdf_lmf.update({
     # To ignore
     "wr"    : lambda wr, lexical_entry: lexical_entry.set_homonymNumber(wr),
 
-    "we"    : lambda we, lexical_entry: lexical_entry.set_note(we, type="restriction", language=config.xml.French),
+    "we"    : lambda we, lexical_entry: lexical_entry.set_note(remove_char(we), type="restriction", language=config.xml.French),
     "wn"    : lambda wn, lexical_entry: lexical_entry.set_note(wn, type="restriction", language=config.xml.English),
 
     # Fonction sémantique (Fr)
@@ -84,10 +84,12 @@ mdf_lmf.update({
     "ur"    : lambda ur, lexical_entry: lexical_entry.set_note(ur, type="usage", language=config.xml.French),
     "uv"    : lambda uv, lexical_entry: lexical_entry.set_note(uv, type="usage", language=config.xml.English),
 
-    "lt"    : lambda lt, lexical_entry: lexical_entry.set_literally(lt, language=config.xml.French),
+    "nt"    : lambda nt, lexical_entry: lexical_entry.set_note(remove_char(nt), type="general"),
+
+    "lt"    : lambda lt, lexical_entry: lexical_entry.set_literally(remove_char(lt), language=config.xml.French),
     "ll"    : lambda ll, lexical_entry: lexical_entry.set_literally(ll, language=config.xml.English),
 
-    "oe"    : lambda oe, lexical_entry: lexical_entry.set_example_comment(oe, language=config.xml.French),
+    "oe"    : lambda oe, lexical_entry: lexical_entry.set_example_comment(remove_char(oe), language=config.xml.French),
     "on"    : lambda on, lexical_entry: lexical_entry.set_example_comment(on, language=config.xml.English),
 
     # Etymological language
@@ -96,13 +98,16 @@ mdf_lmf.update({
     # To ignore
     "dc"    : lambda dc, lexical_entry: None,
 
-    "la"    : lambda la, lexical_entry: lexical_entry.set_citation_form(la),
+    "la"    : lambda la, lexical_entry: lexical_entry.set_citation_form(remove_char(la)),
 
     # Legend (TODO)
     "lg"    : lambda lg, lexical_entry: None,
 
-    "ce"    : lambda ce, lexical_entry: lexical_entry.set_last_related_form(ce, language=config.xml.French),
+    "ce"    : lambda ce, lexical_entry: lexical_entry.set_last_related_form(remove_char(ce), language=config.xml.French),
     "cn"    : lambda cn, lexical_entry: lexical_entry.set_last_related_form(cn, language=config.xml.English),
+    "cf"    : lambda cf, lexical_entry: lexical_entry.create_and_add_related_form(remove_char(cf), mdf_semanticRelation["cf"]),
+
+    "sy"    : lambda sy, lexical_entry: lexical_entry.create_and_add_related_form(remove_char(sy), mdf_semanticRelation["sy"]),
 
     # Hidden (TODO later)
     "u"     : lambda u, lexical_entry: None,
@@ -119,8 +124,10 @@ mdf_lmf.update({
     "ue"    : lambda ue, lexical_entry: lexical_entry.set_usage_note(ue, language=config.xml.French),
     "un"    : lambda un, lexical_entry: lexical_entry.set_usage_note(un, language=config.xml.English),
 
-    "ee" : lambda ee, lexical_entry: lexical_entry.set_encyclopedic_information(ee, language=config.xml.French),
-    "en" : lambda en, lexical_entry: lexical_entry.set_encyclopedic_information(en, language=config.xml.English),
+    "ee"    : lambda ee, lexical_entry: lexical_entry.set_encyclopedic_information(remove_char(ee), language=config.xml.French),
+    "en"    : lambda en, lexical_entry: lexical_entry.set_encyclopedic_information(remove_char(en), language=config.xml.English),
+
+    "sc"    : lambda sc, lexical_entry: lexical_entry.set_scientific_name(remove_char(sc)),
 
     # TODO
     "tb"    : lambda tb, lexical_entry: None,
@@ -142,7 +149,11 @@ mdf_lmf.update({
     # morphèmes
     "m"     : lambda m, lexical_entry: None,
 
-    "a"     : lambda a, lexical_entry: lexical_entry.set_variant_form(a, type="phonetics"),
+    "pl"    : lambda pl, lexical_entry: lexical_entry.set_paradigm(remove_char(pl), grammatical_number=pd_grammaticalNumber["pl"]),
+
+    "a"     : lambda a, lexical_entry: lexical_entry.set_variant_form(remove_char(a), type="phonetics"),
+    "va"    : lambda va, lexical_entry: lexical_entry.set_variant_form(remove_char(va), type="phonetics"),
+    "ph"    : lambda ph, lexical_entry: lexical_entry.set_phonetic_form(remove_char(ph)),
 
     "de"    : lambda de, lexical_entry: lexical_entry.set_definition(remove_char(de), language=config.xml.French),
     "dn"    : lambda dn, lexical_entry: lexical_entry.set_definition(remove_char(dn), language=config.xml.English),
@@ -150,11 +161,12 @@ mdf_lmf.update({
     "ge"    : lambda ge, lexical_entry: lexical_entry.set_gloss(ge, language=config.xml.French),
     "gn"    : lambda gn, lexical_entry: lexical_entry.set_gloss(gn, language=config.xml.English),
 
-    "re"    : lambda re, lexical_entry: lexical_entry.set_translation(re, language=config.xml.French),
+    "re"    : lambda re, lexical_entry: lexical_entry.set_translation(remove_char(re), language=config.xml.French),
     "rn"    : lambda rn, lexical_entry: lexical_entry.set_translation(rn, language=config.xml.English),
 
     "xe"    : lambda xe, lexical_entry: lexical_entry.add_example(remove_char(xe), language=config.xml.French),
     "xn"    : lambda xn, lexical_entry: lexical_entry.add_example(remove_char(xn), language=config.xml.English),
+    "xv"    : lambda xv, lexical_entry: lexical_entry.create_and_add_example(remove_char(xv), language=config.xml.vernacular),
 
     "ve"    : lambda ve, lexical_entry: lexical_entry.set_variant_comment(ve, language=config.xml.French),
     "vn"    : lambda vn, lexical_entry: lexical_entry.set_variant_comment(vn, language=config.xml.English),
